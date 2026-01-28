9c8422bef11bb3df73b88e98f1984612d4a79ff7
Harshita <harshitadlw16@gmail.com>
2026-01-28 16:32:51 +0530

    Hardcode production backend url

--- vite-project/src/api-client.ts
+++ vite-project/src/api-client.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://bookingapp-gqkh.onrender.com";
+const API_BASE_URL ="https://bookingapp-gqkh.onrender.com";
 
 
 import { RegisterFormData } from "./pages/Register";
